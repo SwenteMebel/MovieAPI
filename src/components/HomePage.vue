f6dc7bc10dfccb28e71093b979d5848f64c7ad12
Swen thuis <swentemebel@outlook.com>
2024-01-25 22:03:26 +0100

    edit

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,6 +1,17 @@
 <template>
 <div class="mt-10"> 
-  <h1 class="text-[50px]">Halloww!</h1>
+  <div class="text-[2rem]">
+    <h1 class="text-center font-semibold">Welkom op de Movie search APP :)</h1>
+    <div class="md:grid flex justify-center">
+      <img src="../assets/netflix.jpg" alt="logo" class="rounded-full">
+    </div>
+    <div class="bg-yellow-400 md:mx-10 md:p-2 rounded-t-xl font-serif">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quibusdam nam quaerat natus nobis unde. Sit beatae obcaecati cumque, animi harum impedit minima ex mollitia vel eligendi voluptatem repellendus eveniet asperiores odio accusantium exercitationem dolorem nisi enim vitae aspernatur amet.</p>
+    </div>
+    <div class="bg-gradient-to-r from-sky-900 via-slate-200 to-slate-900 md:mx-10 md:p-2 rounded-b-2xl">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus odio earum corporis ratione culpa doloribus neque omnis consectetur beatae repudiandae!
+    </div>
+  </div>
 </div>
 </template>
 
--- src/router/router.js
+++ src/router/router.js
@@ -10,19 +10,19 @@ const routes = [
         component: Homepage,
     },
 
-    {
+    {   // laadt deze pagina pas in als er in het menu op 'Movie' wordt geklikt 
         path: '/movies',
         name: 'apimovies',
         component: () => import(/*webPackChuckName: "APIMovies" */ '@/components/ApiMovie.vue')
     },
-    {
+    {   // laadt deze pagina pas in als er op een film wordt geklikt. 
         path: '/detailmovie/:id',
         name: 'detailmovie',
         component: () => import (/*webPackChuckName: "DetailsMovie"*/ '@/components/DetailMovie.vue')
     }
 ]
 
-
+// zorgt ervoor dat er geen # in de url balk komt. 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
